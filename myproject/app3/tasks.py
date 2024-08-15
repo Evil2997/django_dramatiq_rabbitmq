97a99862fd49734b90504278f4ca8e5727649456
Evil2997 <dima.molchanow2012@gmail.com>
2024-08-15 16:49:45 +0300

    Update

--- myproject/app3/tasks.py
+++ myproject/app3/tasks.py
@@ -1,6 +1,7 @@
 import logging
 import time
 from django.conf import settings
+import time
 
 import dramatiq
 
@@ -10,25 +11,31 @@ logger = logging.getLogger("django")
 @dramatiq.actor(queue_name="high_priority_queue")
 def high_priority_task(x, y):
     logger.info(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ HIGH PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
+    print(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ HIGH PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
     result = (x + y) * (x - y)
     time.sleep(5)
     logger.info(f"RESULT HIGH PRIORITY: {result}\n{'=' * 40}")
+    print(f"RESULT HIGH PRIORITY: {result}\n{'=' * 40}")
     return result
 
 
 @dramatiq.actor(queue_name="medium_priority_queue")
 def medium_priority_task(x, y):
     logger.info(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ MEDIUM PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
+    print(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ MEDIUM PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
     result = (x + y) * (x - y)
     time.sleep(5)
     logger.info(f"RESULT MEDIUM PRIORITY: {result}\n{'=' * 40}")
+    print(f"RESULT MEDIUM PRIORITY: {result}\n{'=' * 40}")
     return result
 
 
 @dramatiq.actor(queue_name="low_priority_queue")
 def low_priority_task(x, y):
     logger.info(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ LOW PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
+    print(f"\n{'=' * 40}\nЗАПУСК ЗАДАЧИ LOW PRIORITY\nАргументы: x={x}, y={y}\n{'-' * 40}")
     result = (x + y) * (x - y)
     time.sleep(5)
     logger.info(f"RESULT LOW PRIORITY: {result}\n{'=' * 40}")
+    print(f"RESULT LOW PRIORITY: {result}\n{'=' * 40}")
     return result
--- myproject/app3/views.py
+++ myproject/app3/views.py
@@ -1,28 +1,41 @@
 import logging
 
-from django.http import JsonResponse
 from django.conf import settings
+from django.http import JsonResponse
 
 from django.shortcuts import render
-from .tasks import high_priority_task, medium_priority_task, low_priority_task
+from .tasks import *
 import random
 
 logger = logging.getLogger("django")
 
 
+# def start_all_tasks(request):
+#     if request.method == "POST":
+#         logger.info("Запуск всех задач...")
+#
+#         for _ in range(3):
+#             high_priority_task.send(random.randint(1, 10), random.randint(1, 10))
+#
+#         for _ in range(4):
+#             medium_priority_task.send(random.randint(1, 10), random.randint(1, 10))
+#
+#         for _ in range(3):
+#             low_priority_task.send(random.randint(1, 10), random.randint(1, 10))
+#
+#         return JsonResponse({'status': 'All tasks started'})
+#
+#     return render(request, 'app3/start_all_tasks.html')
+
+
 def start_all_tasks(request):
     if request.method == "POST":
         logger.info("Запуск всех задач...")
-        for _ in range(3):
-            high_priority_task.send(random.randint(1, 10), random.randint(1, 10))
 
-        for _ in range(4):
-            medium_priority_task.send(random.randint(1, 10), random.randint(1, 10))
+        # Отправка задачи
+        message = high_priority_task.send(random.randint(1, 10), random.randint(1, 10))
 
-        for _ in range(3):
-            low_priority_task.send(random.randint(1, 10), random.randint(1, 10))
-
-        return JsonResponse({'status': 'All tasks started'})
+        return JsonResponse({'status': 'Task enqueued', 'task_id': message.message_id})
 
     return render(request, 'app3/start_all_tasks.html')
 
